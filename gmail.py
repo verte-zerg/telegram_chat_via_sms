3a5ab47e698eb17a60c5db8ba7bd389c0c470e92
Michail Zhuk <stuntracing@mail.ru>
2020-10-31 06:17:51 +0300

    initial commit

---
+++ gmail.py
@@ -0,0 +1,56 @@
+import pickle
+import os
+import os.path
+from email.mime.text import MIMEText
+import base64
+
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+MAIL_FROM = os.getenv('MAIL_FROM')
+MAIL_TO = os.getenv('MAIL_TO')
+
+def get_or_refresh_service():
+    creds = None
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('gmail', 'v1', credentials=creds)
+
+def create_message(sender, to, subject, message_text):
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+def send_message_api(service, message, user_id='me'):
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message)
+                    .execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+def send_message(text):
+    service = get_or_refresh_service()
+    message = create_message(MAIL_FROM, MAIL_TO, 'Message to SMS', text)
+    send_message_api(service, message)
---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+import smtplib
+from typing import Dict, NamedTuple
+from datetime import datetime
+
+from telethon import TelegramClient, events, utils
+
+from gmail import send_message
+
+
+Message = NamedTuple('Message', [('user', str), ('dt', datetime), ('text', str)])
+
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+
+client = TelegramClient('anon', api_id, api_hash)
+
+messages: Dict[int, Message] = {}
+
+@client.on(events.NewMessage())
+async def handler_new_message(event):
+    message = event.message
+    user = await client.get_entity(message.peer_id)
+    username = utils.get_display_name(user)
+    messages[message.id] = Message(user=username, dt=datetime.now(), text=message.message)
+    res = []
+    res.append(f'[NEW MESSAGE FROM USER {username}][{datetime.now()}]')
+    res.append(message.message)
+    print('\n'.join(res))
+    send_message('\n'.join(res))
+
+
+@client.on(events.MessageEdited())
+async def handler_edit_message(event):
+    message = event.message
+    user = await client.get_entity(message.peer_id)
+    username = utils.get_display_name(user)
+    res = []
+    res.append(f'[EDIT MESSAGE FROM USER {username}][{datetime.now()}]')
+    if message.id in messages:
+        res.append('[OLD_MESSAGE]\n' + messages[message.id].text)
+    else:
+        res.append('[OLD_MESSAGE_NOT_SAVED]')
+
+    messages[message.id] = Message(user=username, dt=datetime.now(), text=message.message)
+    res.append('[NEW_MESSAGE]\n' + messages[message.id].text)
+    print('\n'.join(res))
+    send_message('\n'.join(res))
+
+
+@client.on(events.MessageDeleted())
+async def handler_delete_message(event):
+    first_id = event.deleted_ids[-1]
+    if first_id in messages:
+        username = messages[first_id].user
+    else:
+        username = 'UNKNOWN'
+    res = []
+    res.append(f'[DELETED MESSAGES FROM USER {username}][{datetime.now()}]')
+    for message_id in event.deleted_ids:
+        if message_id in messages:
+            res.append(f'[{messages[message_id].dt}] {messages[message_id].text}')
+        else:
+            res.append(f'MESSAGE WITH ID={message_id} NOT FOUND')
+    print('\n'.join(res))
+    send_message('\n'.join(res))
+
+with client:
+    client.start()
+    client.run_until_disconnected()
